9102a99b4a9cc88eff748fbf7b897ac3fb359353
sebfried <33882012+freitit@users.noreply.github.com>
2022-01-22 15:20:00 +0700

    Added the component TopHeading

---
+++ components/TheHeading/TopHeading.vue
@@ -0,0 +1,66 @@
+<template>
+  <nav class="level has-background-dark has-text-white is-size-7 px-6 py-1 m-0">
+    <div class="level-left" />
+    <div class="level-right">
+      <TopHeadingMenuItem
+        v-for="menuItem in menuList"
+        :key="menuItem.key"
+        class="level-item mx-2"
+        :icon="menuItem.icon"
+        :alt-text="menuItem.key"
+        :title="menuItem.title"
+        :link="menuItem.href"
+        :is-show-number-badge="menuItem.isShowNumberBadge"
+      />
+    </div>
+  </nav>
+</template>
+
+<script>
+import TopHeadingMenuItem from '~/components/TheHeading/TopHeadingMenuItem'
+export default {
+  name: 'TopHeading',
+  components: { TopHeadingMenuItem },
+  data() {
+    return {
+      menuList: [
+        {
+          key: 'order-tracking',
+          icon: require('~/assets/img/order-tracking.svg'),
+          title: 'Tra cứu đơn hàng',
+          href: '#',
+          isShowNumberBadge: false
+        },
+        {
+          key: 'store-location',
+          icon: require('~/assets/img/store-location.svg'),
+          title: 'Tìm cửa hàng',
+          href: '#',
+          isShowNumberBadge: false
+        },
+        {
+          key: 'favourite-heart',
+          icon: require('~/assets/img/favourite-heart.svg'),
+          title: 'Yêu thích',
+          href: '#',
+          isShowNumberBadge: false
+        },
+        {
+          key: 'log-in',
+          icon: require('~/assets/img/log-in.svg'),
+          title: 'Đăng nhập',
+          href: '#',
+          isShowNumberBadge: false
+        },
+        {
+          key: 'cart',
+          icon: require('~/assets/img/cart.svg'),
+          title: 'Giỏ hàng',
+          href: '#',
+          isShowNumberBadge: true
+        },
+      ],
+    }
+  },
+}
+</script>
---
+++ components/TheHeading/TopHeadingMenuItem.vue
@@ -0,0 +1,62 @@
+<template>
+  <a :href="link">
+    <span class="icon mr-1">
+      <img :src="icon" :alt="altText" />
+    </span>
+    {{ title }}
+    <span v-if="isShowNumberBadge" class="ml-1">({{ getNumberBadge }})</span>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'TopHeadingMenuItem',
+  props: {
+    altText: {
+      type: String,
+      required: true,
+      default: ""
+    },
+    icon: {
+      type: String,
+      required: true,
+      default: ""
+    },
+    link: {
+      type: String,
+      required: true,
+      default: ""
+    },
+    title: {
+      type: String,
+      required: true,
+      default: ""
+    },
+    isShowNumberBadge: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      alo: 123
+    };
+  },
+  computed: {
+    getNumberBadge(){
+      // get number from vuex store and return
+      return 0;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+a {
+  color: $info;
+
+  &:hover {
+    color: $primary;
+  }
+}
+</style>
