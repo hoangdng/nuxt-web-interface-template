dd5c45e74b206f47224532f6023f91089e5d4f9b
hoang.tran-nguyen <hoang.tran-nguyen@soget.fr>
2022-01-23 23:46:54 +0700

    Create component RightSideBar

---
+++ components/RightSideBar/RightSideBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hide-in-tablet-screen">
+    <CartFixed/>
+    <SocialsFixed/>
+  </div>
+</template>
+
+<script>
+import CartFixed from "@/components/RightSideBar/CartFixed.vue";
+import SocialsFixed from "@/components/RightSideBar/SocialsFixed.vue";
+
+export default {
+  name: "RightSideBar",
+  components: {CartFixed, SocialsFixed},
+}
+</script>
+
+<style scoped>
+@media only screen and (max-width: 768px) {
+  .hide-in-tablet-screen {
+    display: none;
+  }
+}
+</style>
