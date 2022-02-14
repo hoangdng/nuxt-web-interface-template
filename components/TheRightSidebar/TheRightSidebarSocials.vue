7ac6ff16dd7ec0597188cf1dac27923fd40cccf5
hoangdng <62456341+hoangdng@users.noreply.github.com>
2022-02-14 09:20:10 +0700

    AB#6: Create components TheFooter  (#10)
    * Edit dark color
    
    * Create component TheFooter
    
    * Add component TheFooter to default layout
    
    * Add package style-resources to auto import scss files
    
    * Remove importing scss lines
    
    Co-authored-by: hoang.tran-nguyen <hoang.tran-nguyen@soget.fr>

--- components/TheRightSidebar/TheRightSidebarSocials.vue
+++ components/TheRightSidebar/TheRightSidebarSocials.vue
@@ -32,8 +32,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "assets/customized-buefy.scss";
-
 a {
   color: $grey-light;
 }
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-          <img src="~assets/buefy.png" alt="Buefy" height="28" />
-        </a>
+        <nav
+          class="navbar header has-shadow is-primary"
+          role="navigation"
+          aria-label="main navigation"
+        >
+          <div class="navbar-brand">
+            <a class="navbar-item" href="/">
+              <img src="~assets/buefy.png" alt="Buefy" height="28"/>
+            </a>
 
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </nav>
+            <div class="navbar-burger">
+              <span/>
+              <span/>
+              <span/>
+            </div>
+          </div>
+        </nav>
 
     <section class="main-content columns">
       <TheRightSidebar/>
@@ -25,16 +25,17 @@
         <ul class="menu-list">
           <li v-for="(item, key) of items" :key="key">
             <NuxtLink :to="item.to" exact-active-class="is-active">
-              <b-icon :icon="item.icon" /> {{ item.title }}
+              <b-icon :icon="item.icon"/>
+              {{ item.title }}
             </NuxtLink>
           </li>
         </ul>
       </aside>
-
       <div class="container column is-10 test">
         <Nuxt />
       </div>
     </section>
+    <TheFooter/>
   </div>
 </template>
 
@@ -47,12 +48,12 @@ export default {
         {
           title: 'Home',
           icon: 'home',
-          to: { name: 'index' },
+          to: {name: 'index'},
         },
         {
           title: 'Inspire',
           icon: 'lightbulb',
-          to: { name: 'inspire' },
+          to: {name: 'inspire'},
         },
       ],
     }
