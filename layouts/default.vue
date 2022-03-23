46057d12a06d5638bc0814ad835ab8d7d591e057
hoangdng <62456341+hoangdng@users.noreply.github.com>
2022-03-23 15:47:01 +0700

    Edit default layout file (#47)
    Co-authored-by: hoang.tran-nguyen <hoang.tran-nguyen@soget.fr>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,56 +1,18 @@
 <template>
   <div>
     <TheHeader />
-
-    <section class="main-content columns">
-      <TheRightSidebar/>
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">General</p>
-        <ul class="menu-list">
-          <li v-for="(item, key) of items" :key="key">
-            <NuxtLink :to="item.to" exact-active-class="is-active">
-              <b-icon :icon="item.icon"/>
-              {{ item.title }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </aside>
-      <div class="container column is-10 test">
-        <Nuxt />
-      </div>
-    </section>
-    
-    <TheFooter/>
+    <TheRightSidebar />
+    <TheFooter />
   </div>
 </template>
 
 <script>
-import TheHeader from "~/components/TheHeader/TheHeader";
+import TheHeader from '~/components/TheHeader/TheHeader'
+import TheRightSidebar from '~/components/TheRightSidebar/TheRightSidebar'
+import TheFooter from '~/components/TheFooter'
 
 export default {
   name: 'DefaultLayout',
-  components: { TheHeader },
-  data() {
-    return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: {name: 'index'},
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: {name: 'inspire'},
-        },
-      ],
-    }
-  },
+  components: { TheHeader, TheRightSidebar, TheFooter }
 }
 </script>
-
-<style>
-.test{
-  height: 1000px;
-}
-</style>
