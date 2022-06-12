4e57d0baccc4c6543511a65f981171356f33b15b
hoangdng <62456341+hoangdng@users.noreply.github.com>
2022-06-12 16:37:41 +0700

    Fixed AB#15: Create component to display categories in home page (#83)
    * Create component for displaying product categories on home page
    
    * Modify index page to contain newly created component
    
    Co-authored-by: hoang.tran-nguyen <hoang.tran-nguyen@soget.fr>
    Co-authored-by: freitit <33882012+freitit@users.noreply.github.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,19 @@
 <template>
   <div>
     <TheHeader />
+    <Nuxt />
     <TheRightSidebar />
     <TheFooter />
   </div>
 </template>
 
 <script>
-import TheHeader from '~/components/TheHeader/TheHeader'
-import TheRightSidebar from '~/components/TheRightSidebar/TheRightSidebar'
-import TheFooter from '~/components/TheFooter'
+import TheHeader from "~/components/TheHeader/TheHeader";
+import TheRightSidebar from "~/components/TheRightSidebar/TheRightSidebar";
+import TheFooter from "~/components/TheFooter";
 
 export default {
-  name: 'DefaultLayout',
+  name: "DefaultLayout",
   components: { TheHeader, TheRightSidebar, TheFooter }
 }
 </script>
