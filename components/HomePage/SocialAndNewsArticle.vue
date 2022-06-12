19c5684eacf8417a705ac8adc8365528378d827f
hoangdng <62456341+hoangdng@users.noreply.github.com>
2022-06-12 16:55:19 +0700

    Create component to display articles on the home page (#89)
    Co-authored-by: hoang.tran-nguyen <hoang.tran-nguyen@soget.fr>
    Co-authored-by: freitit <33882012+freitit@users.noreply.github.com>

---
+++ components/HomePage/SocialAndNewsArticle.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="column is-half">
+    <div>
+      <nuxt-link :to="`/articles/${title}`">
+        <b-image :src="thumbnailUrl" ratio="16by9"/>
+        <h2 class="is-uppercase is-size-5 is-size-6-touch has-text-weight-bold my-2">
+          {{ title }}
+        </h2>
+      </nuxt-link>
+      <p>{{ content }}</p>
+      <nuxt-link :to="`/articles/${title}`">
+        <h4 class="has-text-primary is-italic has-text-weight-semibold has-text-right mx-4">
+          Đọc thêm
+        </h4>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SocialAndNewsArticle",
+  props: {
+    thumbnailUrl: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+p {
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+}
+
+a {
+  color: black;
+}
+
+figure:hover {
+  transform: scale(1.02);
+  transition: all 0.3s;
+}
+
+h2:hover {
+  color: $primary;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,21 @@
   <section class="section">
     <ProductCategories />
     <ListBestSeller />
+    <SocialAndNews />
   </section>
 </template>
 
 <script>
 import ListBestSeller from "~/components/HomePage/ListBestSeller";
 import ProductCategories from "~/components/HomePage/ProductCategories";
+import SocialAndNews from "~/components/HomePage/SocialAndNews";
 
 export default {
   name: "IndexPage",
   components: {
     ListBestSeller,
-    ProductCategories
+    ProductCategories,
+    SocialAndNews
   },
 }
 </script>
