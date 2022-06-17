c29f8a4faff842d1c367fd3f67a3b459cc935ee9
Vu, Tran Van <33882012+freitit@users.noreply.github.com>
2022-06-17 13:28:03 +0700

    Fixed AB#13, fixed AB#14: Page banner and special collections display sections (#96)
    * refactor code
    
    * add home page
    
    * update homepage

---
+++ components/HomePage/SpecialCollections.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div class="columns is-variable is-5 m-6 is-align-items-stretch">
+      <div class="column is-flex is-align-items-stretch">
+        <div class="card">
+          <div class="card-image">
+            <b-image
+              class="image"
+              src="https://ananas.vn/wp-content/uploads/banner-phụ_2m-600x320.jpg"
+            />
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">ALL BLACK IN BLACK</p>
+              </div>
+            </div>
+
+            <div class="content">
+              <p>
+                Mặc dù được ứng dụng rất nhiều, nhưng sắc đen lúc nào cũng toát
+                lên một vẻ huyền bí không nhàm chán
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="column is-flex is-align-items-stretch">
+        <div class="card">
+          <div class="card-image">
+            <b-image
+              class="image"
+              src="https://ananas.vn/wp-content/uploads/Banner_Sale-off-1.jpg"
+            />
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">OUTLET SALE</p>
+              </div>
+            </div>
+
+            <div class="content">
+              Danh mục những sản phẩm bán tại "giá tốt hơn" chỉ được bán kênh
+              online - Online Only, chúng đã từng làm mưa làm gió một thời gian
+              và hiện đang rơi vào tình trạng bể size, bể số.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SpecialCollections"
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,35 @@
 <template>
-  <section class="section">
-    <ProductCategories />
-    <ListBestSeller />
-    <SocialAndNews />
-  </section>
+  <div>
+    <TopBanner />
+    <section class="section">
+      <SpecialCollections />
+      <ProductCategories />
+      <ListBestSeller />
+    </section>
+    <div class="hero">
+      <b-image class="image" src="https://ananas.vn/wp-content/uploads/Banner_Clothing.jpg" alt="ananas banner" />
+    </div>
+    <section class="section">
+      <SocialAndNews />
+    </section>
+  </div>
 </template>
 
 <script>
 import ListBestSeller from "~/components/HomePage/ListBestSeller";
 import ProductCategories from "~/components/HomePage/ProductCategories";
 import SocialAndNews from "~/components/HomePage/SocialAndNews";
+import SpecialCollections from "~/components/HomePage/SpecialCollections";
+import TopBanner from "~/components/HomePage/TopBanner";
 
 export default {
   name: "IndexPage",
   components: {
     ListBestSeller,
     ProductCategories,
-    SocialAndNews
+    SocialAndNews,
+    SpecialCollections,
+    TopBanner
   },
 }
 </script>
